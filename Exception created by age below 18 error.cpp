3ef58600e0ced4820730695802128df4a62fc0bd
Siddhant Bhosale <siddhant.bhosale05@gmail.com>
2024-10-09 15:34:56 +0530

    Add files via upload

---
+++ Exception created by age below 18 error.cpp
@@ -0,0 +1,49 @@
+//Name:Siddhant Kiran Bhosale
+//PRN:23070123038
+//Aim:Implemention of Exception Handling to perform Handle Exception created by age below 18 error
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float age;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    try {
+        if (age < 18) {
+            throw age; 
+        } 
+        else if (age < 0) { 
+            throw age; 
+        } 
+        else {
+            cout << "Accepted" << endl;
+        }
+    }
+    catch (float age) {
+        if (age < 0) {
+            cout << "\nInvalid age"<<endl;
+        } 
+        else {
+            cout << "\nYou are below 18, Not Accepted" << endl; 
+        }
+    }
+
+    return 0;
+}
+
+
+/*Output:
+Enter your age: 19
+Accepted
+For age below 18-
+Enter your age: 17
+
+You are below 18, Not Accepted
+For negative age-
+Enter your age: -2
+
+Invalid age
+*/
